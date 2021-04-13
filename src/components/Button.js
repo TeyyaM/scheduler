95d46c86616fc4840a715c0de5223753f886e94f
Tessa Azar MacLean <74215019+TeyyaM@users.noreply.github.com>
2021-04-12 17:11:09 -0700

    Button component complete

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,23 @@
 import React from "react";
-
 import "components/Button.scss";
 
+const classNames = require('classnames');
+
 export default function Button(props) {
-   return <></>;
+   const { confirm, danger, onClick, disabled, children } = props;
+   const buttonClass = classNames(
+      'button',
+      { ' button--confirm': confirm },
+      { ' button--danger': danger }
+   );
+
+   return (
+      <button
+         className={buttonClass}
+         onClick={onClick}
+         disabled={disabled}
+      >
+         {children}
+      </button>
+   );
 }
