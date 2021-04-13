3f4725935c6566196d75da299ee97621a0b2dda2
Tessa Azar MacLean <74215019+TeyyaM@users.noreply.github.com>
2021-04-12 17:12:04 -0700

    Set up for DayListItem, no classes

---
+++ src/components/DayListItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+  const { name, spots, selected, setDay } = props;
+  return (
+    <li onClick={props.setDay(name)} selected={selected}>
+      <h2 className="text--regular">{name}</h2>
+      <h3 className="text--light">{spots} spots remaining</h3>
+    </li>
+  );
+}
