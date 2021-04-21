3733318099f1e0acf8c34f6d3a49f0fa2611fea5
Tessa Azar MacLean <74215019+TeyyaM@users.noreply.github.com>
2021-04-20 18:16:08 -0700

    Add further tests, mocking using axios to create or delete an appointment

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -5,12 +5,58 @@ import { render, cleanup, waitForElement, fireEvent } from "@testing-library/rea
 import Application from "components/Application";
 
 afterEach(cleanup);
+describe("Application", () => {
+  it("changes the schedule when a new day is selected", async () => {
+    const { getByText } = render(<Application />);
 
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
-  const { getByText } = render(<Application />);
+    await waitForElement(() => getByText("Monday"));
 
-  return waitForElement(() => getByText("Monday")).then(() => {
     fireEvent.click(getByText("Tuesday"));
+
     expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
+
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+
+    const { getByText, getByAltText, getByPlaceholderText, getAllByAltText, queryByText } = render(<Application />);
+
+    await waitForElement(() => getByText("Archie Cohen"));
+
+    fireEvent.click(getAllByAltText("Add")[0]);
+
+    fireEvent.change(getByPlaceholderText("Enter Student Name"), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+
+    fireEvent.click(getByAltText("Sylvia Palmer"));
+
+    fireEvent.click(getByText("Save"));
+
+    expect(queryByText(/saving/i)).not.toBeNull();
+
+    await waitForElement(() => getByText("Lydia Miller-Jones"));
+
+    expect(queryByText(/saving/i)).toBeNull();
+
+    expect(getByText("no spots remaining")).toBeInTheDocument();
+  });
+
+  it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+
+    const { getByText, getByAltText, queryByText } = render(<Application />);
+
+    await waitForElement(() => getByText("Archie Cohen"));
+
+    fireEvent.click(getByAltText("Delete"));
+
+    fireEvent.click(getByText("Confirm"));
+
+    await waitForElement(() => getByText("Deleting"));
+
+    await waitForElement(() => getByText("2 spots remaining"));
+
+    expect(queryByText(/deleting/i)).toBeNull();
+
+    expect(getByText("2 spots remaining")).toBeInTheDocument();
   });
 });
